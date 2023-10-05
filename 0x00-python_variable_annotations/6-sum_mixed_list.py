cc187e365c8034506997645a09a089fa9788c13e
atck3r <atck3r@localdomain>
2023-10-05 23:38:56 +0300

    Fixed

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,6 +3,7 @@
 '''
 from typing import Union, List
 
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     '''Sum list of int and floar'''
     return float(sum(mxd_lst))
--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,6 +4,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''Duple the v variable'''
-    return (k, v*v)
+    return (k, float(v**2))
